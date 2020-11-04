9668253ff31653cefec0b01bb042465506a83f1b
czp3009 <czp3009@gmail.com>
2020-11-04 22:27:47 +0800

    2020-08-22

---
+++ Data/Scripts/WeaponThread/MasterConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace WeaponThread
+{
+    partial class Weapons
+    {
+        internal Weapons()
+        {
+            // file convention: Name.cs - See Example.cs file for weapon property details.
+            //
+            // Enable your config files using the follow syntax, don't include the ".cs" extension:
+            // ConfigFiles(Your1stConfigFile, Your2ndConfigFile, Your3rdConfigFile);
+
+            ConfigFiles (
+                            HeavyDefenseTurret,
+                            CoilCannon,
+                            BattleshipCannon,
+                            BattleshipCannonMK2,
+                            BattleshipCannonMK3,
+                            Static88mm,
+                            Static150mm,
+                            TelionAFGen1,
+                            TelionAFGen2,
+                            TelionAMACM,
+                            BFTriCannon,
+                            TelionOctaBattery,
+                            TelionDualBattery
+
+                            );//XB1BeamBattery, XB3BeamBattery, BattleshipCannon
+        }
+    }
+}
